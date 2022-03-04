3f3615d806bb2cb8b9d1668a3e496864ac2bde58
Hajime Kawahara <divrot@gmail.com>
2022-03-04 17:02:20 +0900

    gen dynamic lc

--- src/jaxsot/core/lc.py
+++ src/jaxsot/core/lc.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-
 def gen_lightcurve(W,mmap,sigma_relative):
-    """light curve generator
+    """light curve generator using a static map
+
+    Args:
+       W: weight matrix
+       mmap: static healpix map
+       sigma_relative: relative noise level
+
+    Returns:
+       lightcurve
 
     """
     lc=np.dot(W,mmap)
@@ -11,30 +18,22 @@ def gen_lightcurve(W,mmap,sigma_relative):
     lc=lc+noise
     return lc
 
+def gen_dynamic_lightcurve(W,M,sigma_relative):
+    """light curve generator using a dynamic map
 
-if __name__=="__main__":
-    from jaxsot.core.weight import comp_weight, comp_omega
-    from jaxsot.io.earth import binarymap
+    Args:
+       W: weight matrix
+       M: dynamic map
+       sigma_relative: relative noise level
 
-    mmap=binarymap(nside=16,show=False)
+    Returns:
+       lightcurve
 
-    nside=16
-    inc=0.0
-    Thetaeq=np.pi
-    zeta=np.pi/3.0
-    Pspin=23.9344699/24.0
-    wspin=2*np.pi/Pspin
-    Porb=40.0
-    worb=2.*np.pi/Porb
-    N=1024
-    obst=np.linspace(0.0,Porb,N)
-    Thetav=worb*obst
-    Phiv=np.mod(wspin*obst,2*np.pi)
-    omega=comp_omega(nside)
-    WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
-    W=WI*WV
-    lc=gen_lightcurve(W,mmap,0.0)
-    print(np.sum(lc))
-    import matplotlib.pyplot as plt
-    plt.plot(obst,lc)
-    plt.show()
+    """
+
+    lc=np.sum(W*M,axis=1)
+    sigma=np.mean(lc)*sigma_relative
+    noise=np.random.normal(0.0,sigma,len(lc))
+    lc=lc+noise
+    return lc
+
--- src/jaxsot/mockdata/testdata.py
+++ src/jaxsot/mockdata/testdata.py
@@ -33,3 +33,32 @@ def static_singleband(nside):
     W=jnp.array(WI*WV)
     lc=gen_lightcurve(W,mmap,0.1)
     return lc, W
+
+def dynamic_singleband(nside):
+    """generate test data for a single band observation of a rotating geography
+
+    Args:
+        nside: nside of Healpix
+
+    Returns: 
+        test light curve, weight matrix
+ 
+    """
+    mmap=binarymap(nside=nside,show=False)
+    inc=0.0
+    Thetaeq=np.pi
+    zeta=np.pi/3.0
+    Pspin=23.9344699/24.0
+    wspin=2*np.pi/Pspin
+    Porb=40.0
+    worb=2.*np.pi/Porb
+    N=1024
+    obst=np.linspace(0.0,Porb,N)
+    Thetav=worb*obst
+    Phiv=np.mod(wspin*obst,2*np.pi)
+    
+    omega=comp_omega(nside)
+    WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
+    W=jnp.array(WI*WV)
+    lc=gen_lightcurve(W,mmap,0.1)
+    
