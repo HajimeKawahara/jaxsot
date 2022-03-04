ef8e2cf601c5da521fc9938a95ffc38f84cac1af
Hajime Kawahara <divrot@gmail.com>
2022-03-04 11:27:58 +0900

    made separate testdata module and a unit test for L1+TSV

--- examples/sotl1tsv.py
+++ examples/sotl1tsv.py
@@ -1,32 +1,15 @@
 import numpy as np
 import jax.numpy as jnp
-from jaxsot.core.weight import comp_weight, comp_omega
-from jaxsot.core.lc import gen_lightcurve
-from jaxsot.core.neighbor import calc_neighbor_weightmatrix
-from jaxsot.io.earth import binarymap
 import jaxopt
 import healpy as hp
 import matplotlib.pyplot as plt
+from jaxsot.core.testdata import static_singleband
+from jaxsot.core.neighbor import calc_neighbor_weightmatrix
 
+np.random.seed(1)
 nside=16
-mmap=binarymap(nside=nside,show=False)
-inc=0.0
-Thetaeq=np.pi
-zeta=np.pi/3.0
-Pspin=23.9344699/24.0
-wspin=2*np.pi/Pspin
-Porb=40.0
-worb=2.*np.pi/Porb
-N=1024
-obst=np.linspace(0.0,Porb,N)
-Thetav=worb*obst
-Phiv=np.mod(wspin*obst,2*np.pi)
-
-omega=comp_omega(nside)
-WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
-W=jnp.array(WI*WV)
-lc=gen_lightcurve(W,mmap,0.1)
-
+npix=hp.nside2npix(nside)
+lc,W=static_singleband(nside)
 wtsv, _ = calc_neighbor_weightmatrix(nside)
 
 def objective(params,lamtsv):
@@ -36,7 +19,7 @@ def objective(params,lamtsv):
 
 #pg = jaxopt.ProximalGradient(fun=objective, prox=jaxopt.prox.prox_lasso, maxiter=5000)
 pg = jaxopt.ProximalGradient(fun=objective, prox=jaxopt.prox.prox_non_negative_lasso, maxiter=5000)
-res = pg.run(init_params=np.random.normal(0.0,1.0,len(mmap)),  hyperparams_prox=0.01, lamtsv=0.01)
+res = pg.run(init_params=np.random.normal(0.0,1.0,npix),  hyperparams_prox=0.01, lamtsv=0.01)
 params, state = res
 hp.mollview(params,flip="geo")
 plt.show()
---
+++ src/jaxsot/core/testdata.py
@@ -0,0 +1,35 @@
+import numpy as np
+import jax.numpy as jnp
+from jaxsot.core.weight import comp_weight, comp_omega
+from jaxsot.core.lc import gen_lightcurve
+from jaxsot.io.earth import binarymap
+
+def static_singleband(nside):
+    """generate test data for a single band observation of a static geography
+
+    Args:
+        nside: nside of Healpix
+
+    Returns: 
+        test light curve, weight matrix
+ 
+    """
+    
+    mmap=binarymap(nside=nside,show=False)
+    inc=0.0
+    Thetaeq=np.pi
+    zeta=np.pi/3.0
+    Pspin=23.9344699/24.0
+    wspin=2*np.pi/Pspin
+    Porb=40.0
+    worb=2.*np.pi/Porb
+    N=1024
+    obst=np.linspace(0.0,Porb,N)
+    Thetav=worb*obst
+    Phiv=np.mod(wspin*obst,2*np.pi)
+    
+    omega=comp_omega(nside)
+    WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
+    W=jnp.array(WI*WV)
+    lc=gen_lightcurve(W,mmap,0.1)
+    return lc, W
