9ad03e9c3f3774f7acd54162ec1e25f7946bc834
Hajime Kawahara <divrot@gmail.com>
2022-04-01 12:13:21 +0900

    GD test of for QP

--- tests/jaxopt/qp_test.py
+++ tests/jaxopt/qp_test.py
@@ -15,6 +15,8 @@ def test_spd():
     gd = jaxopt.GradientDescent(fun=f, maxiter=500)
     res = gd.run(init_params=np.random.rand(N),A=A,b=b)
     print(res.params)
+    ref=12.169304
+    assert np.abs(np.sum(res.params)-ref) == 0.0
     
 if __name__=="__main__":
     test_spd()
