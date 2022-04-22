b943a399e551e95353758f374ffee9f661d46f9b
atsuki-kuwata <a.kuwata@astron.s.u-tokyo.ac.jp>
2022-04-22 14:28:33 +0900

    fixed block_coordinate_descent.py

--- src/jaxsot/core/block_coordinate_descent.py
+++ src/jaxsot/core/block_coordinate_descent.py
@@ -2,12 +2,8 @@ import numpy as np
 import jax.numpy as jnp
 import jaxopt
 
-def QP_obj_xk(params,W_x,K,bx,shapeA0):
-    obj = 0.5*jnp.dot(params, jnp.dot((W_x+K),params)) + jnp.dot(bx,params)
-    return obj/shapeA0
-
-def QP_obj_ak(params,W_a,T_a,b,shapeA0):
-    obj = 0.5*jnp.dot(params, jnp.dot((W_a+T_a),params)) + jnp.dot(b,params)
+def QP_objective(params,W,b,shapeA0):
+    obj = 0.5*jnp.dot(params, jnp.dot(W,params)) + jnp.dot(b,params)
     return obj/shapeA0
 
 def opt_ref_vr(k,Y,W,A,X,lamX):
@@ -38,9 +34,9 @@ def opt_ref_vr(k,Y,W,A,X,lamX):
     K=np.eye(Nl) - np.dot(np.dot(Xminus.T,XXTinverse),Xminus)
     K=K*np.linalg.det(np.dot(Xminus,Xminus.T))*lamX   
 
-    pg = jaxopt.ProjectedGradient(fun=QP_obj_xk, projection=jaxopt.projection.projection_non_negative)
+    pg = jaxopt.ProjectedGradient(fun=QP_objective, projection=jaxopt.projection.projection_non_negative)
     state=pg.init_state(init_params=jnp.array(X[k,:]))
-    params,state=pg.update(params=jnp.array(X[k,:]),state=state,W_x=W_x,K=K,bx=bx,shapeA0=A.shape[0])
+    params,state=pg.update(params=jnp.array(X[k,:]),state=state,W=W_x+K,b=bx,shapeA0=A.shape[0])
 
     return params
 
@@ -68,9 +64,8 @@ def opt_map_l2(k,Y,W,A,X,lamA):
     b=-np.dot(np.dot(W.T,Delta),xk)
     T_a=lamA*np.eye(Nj)
 
-    pg = jaxopt.ProjectedGradient(fun=QP_obj_ak, projection=jaxopt.projection.projection_non_negative)
-    #res = pg.run(init_params=jnp.array(A[:,k]))
+    pg = jaxopt.ProjectedGradient(fun=QP_objective, projection=jaxopt.projection.projection_non_negative)
     state=pg.init_state(init_params=jnp.array(A[:,k]))
-    params,state=pg.update(params=jnp.array(A[:,k]),state=state,W_a=W_a,T_a=T_a,b=b,shapeA0=A.shape[0])
+    params,state=pg.update(params=jnp.array(A[:,k]),state=state,W=W_a+T_a,b=b,shapeA0=A.shape[0])
 
     return params
