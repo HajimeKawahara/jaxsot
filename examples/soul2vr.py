f8e8a37e370176d43d8d5f8e65c5318028036ddb
atsuki-kuwata <a.kuwata@astron.s.u-tokyo.ac.jp>
2022-06-15 12:00:31 +0900

    modified module to make multiband test data

--- examples/soul2vr.py
+++ examples/soul2vr.py
@@ -1,43 +1,14 @@
 import numpy as np
 import jax.numpy as jnp
-from jaxsot.core.weight import comp_weight, comp_omega
-from jaxsot.core.lc import gen_lightcurve
 from jaxsot.core.block_coordinate_descent import opt_map_l2, opt_ref_vr
-from jaxsot.io.earth import binarymap
-from jaxsot.io.earth import multibandmap
+from jaxsot.core.testdata import static_multiband
 import healpy as hp
 import matplotlib.pyplot as plt
 import tqdm
 
 np.random.seed(34)
-
-mmap,Ainit,Xinit=multibandmap(show=False)
-npix=mmap.shape[0]
-nside=hp.npix2nside(npix)
-
-inc=0.0
-Thetaeq=np.pi
-zeta=np.pi/3.0
-Pspin=23.9344699/24.0
-wspin=2*np.pi/Pspin
-Porb=40.0
-worb=2.*np.pi/Porb
-N=1024
-obst=np.linspace(0.0,Porb,N)
-
-Thetav=worb*obst
-Phiv=np.mod(wspin*obst,2*np.pi)
-omega=comp_omega(nside)
-
-WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
-W=np.array(WI*WV)
-lc=gen_lightcurve(W,mmap,0.1)
-
-nside=16
-npix=hp.nside2npix(nside)
-omega=comp_omega(nside)
-WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
-W=jnp.array(WI*WV)
+lc, W = static_multiband()
+npix=W.shape[1]
 Nk=3
 
 A0=np.random.rand(npix,Nk)
--- src/jaxsot/core/testdata.py
+++ src/jaxsot/core/testdata.py
@@ -1,8 +1,9 @@
 import numpy as np
 import jax.numpy as jnp
+import healpy as hp
 from jaxsot.core.weight import comp_weight, comp_omega
 from jaxsot.core.lc import gen_lightcurve
-from jaxsot.io.earth import binarymap
+from jaxsot.io.earth import binarymap, multibandmap
 
 def static_singleband(nside):
     """generate test data for a single band observation of a static geography
@@ -33,3 +34,41 @@ def static_singleband(nside):
     W=jnp.array(WI*WV)
     lc=gen_lightcurve(W,mmap,0.1)
     return lc, W
+
+def static_multiband():
+    """generate test data for a multi-band observation of a static geography
+    
+    Returns:
+        test light curve, weight matrix
+    
+    """
+    
+    mmap,Ainit,Xinit=multibandmap(show=False)
+    npix=mmap.shape[0]
+    nside=hp.npix2nside(npix)
+
+    inc=0.0
+    Thetaeq=np.pi
+    zeta=np.pi/3.0
+    Pspin=23.9344699/24.0
+    wspin=2*np.pi/Pspin
+    Porb=40.0
+    worb=2.*np.pi/Porb
+    N=1024
+    obst=np.linspace(0.0,Porb,N)
+
+    Thetav=worb*obst
+    Phiv=np.mod(wspin*obst,2*np.pi)
+    omega=comp_omega(nside)
+
+    WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
+    W=np.array(WI*WV)
+    lc=gen_lightcurve(W,mmap,0.1)
+
+    nside=16
+    #npix=hp.nside2npix(nside)
+    omega=comp_omega(nside)
+    WI,WV=comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv,omega)
+    W=jnp.array(WI*WV)
+
+    return lc, W 
