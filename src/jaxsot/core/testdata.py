4ab367b31eca6648c66cd50c8e28fad675ccba53
atsuki-kuwata <a.kuwata@astron.s.u-tokyo.ac.jp>
2022-08-13 09:53:28 +0900

    added function for genetaring toy model

--- src/jaxsot/core/testdata.py
+++ src/jaxsot/core/testdata.py
@@ -2,7 +2,7 @@ import numpy as np
 import jax.numpy as jnp
 import healpy as hp
 from jaxsot.core.weight import comp_weight, comp_omega
-from jaxsot.core.lc import gen_lightcurve
+from jaxsot.core.lc import gen_lightcurve, gen_dynamic_lightcurve
 from jaxsot.io.earth import binarymap, multibandmap
 
 def static_singleband(nside):
@@ -69,6 +69,48 @@ def static_multiband(zeta = np.pi / 3.0,
     WI, WV = comp_weight(nside, zeta, inc, Thetaeq, Thetav, Phiv, omega)
     W = np.array(WI * WV)
     lc = gen_lightcurve(W, mmap, 0.1)
+
+    nside = 16
+    #npix = hp.nside2npix(nside)
+    omega = comp_omega(nside)
+    WI, WV = comp_weight(nside, zeta, inc, Thetaeq, Thetav, Phiv, omega)
+    W = jnp.array(WI * WV)
+
+    return lc, W
+
+def dynamic_multiband(zeta = np.pi / 3.0,
+                     inc = 0.0,
+                     Thetaeq = np.pi,
+                     Pspin = 23.9344699 / 24.0,
+                     Porb = 40.0,
+                     N = 1024):
+    """generate test data for a multi-band observation of a static geography
+    Args:
+        zeta: obliquity
+        inc: orbital inclination
+        Thetaeq: orbital phase angle at the vernal equinox
+        Pspin: rotation period
+        Porb: orbital period
+        N: number of observation points
+    Returns:
+        test light curve, weight matrix
+    
+    """
+    mmap, Ainit, Xinit = multibandmap(show=False)
+    
+    npix = mmap.shape[0]
+    nside = hp.npix2nside(npix)
+
+    wspin = 2 * np.pi / Pspin
+    worb = 2. * np.pi / Porb
+    obst = np.linspace(0.0, Porb, N)
+    Thetav = worb * obst
+    Phiv = np.mod(wspin * obst, 2 * np.pi)
+    omega = comp_omega(nside)
+
+    WI, WV = comp_weight(nside, zeta, inc, Thetaeq, Thetav, Phiv, omega)
+    W = np.array(WI * WV)
+    lc = gen_dynamic_lightcurve(W, Ainit, Xinit, obst, 0.1)
 
     nside = 16
     #npix = hp.nside2npix(nside)
